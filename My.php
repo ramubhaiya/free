fdcc840592628bd337aa85c7edfcac29de70c787
ramubhaiya <48592197+ramubhaiya@users.noreply.github.com>
2020-09-15 20:22:46 +0530

    Create My.php

---
+++ My.php
@@ -0,0 +1,12 @@
+
+  
+<?php
+$tags = get_meta_tags('$_POST["yt"]');
+echo $tags['author'];       // name
+echo $tags['keywords'];     // php documentation
+echo $tags['description'];  // a php manual
+echo $tags['geo_position']; // 49.33;-86.59
+?>
+    
+    
+ 
